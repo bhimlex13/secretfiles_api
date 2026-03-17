1e211fbc908afa9842e7b76fc71a67bf0917758d
bhimlex13 <oroalexander004@gmail.com>
2026-03-17 08:47:48 +0800

    update server.js

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/', (req, res) => {
   res.send('Secret Files API is running');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
